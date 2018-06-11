e3a4aeb5e7a17d00d2ab068292a745ada751a1af
Kyle Hoernlein <khoernlein90@gmail.com>
2018-06-11 17:18:29 -0400

    added some dummy data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Test Recipe",
+      "This is a test",
+      "https://www.facebook.com/images/fb_icon_325x325.png"
+    ),
+    new Recipe(
+      "Test Recipe",
+      "This is a test",
+      "https://www.facebook.com/images/fb_icon_325x325.png"
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
